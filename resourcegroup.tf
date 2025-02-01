f207d94f2d891f225e3eccff45624cc4afb6be43
mostafaBachir <mostafa.bachir@gmail.com>
2025-01-31 19:28:12 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "mcit420zz5um" {
   name     = "mcit_resource_group_w24bachir"
   location = "canadacentral"
 }
-resource "azurerm_resource_group" "mcit420zz5um2" {
+/*resource "azurerm_resource_group" "mcit420zz5um2" {
   name     = "mcit_resource_group_w24_2bachir"
   location = "canadacentral"
-}
+}*/
